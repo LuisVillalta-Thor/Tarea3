cebf679874637ea81acc89ca7b9b43a5748d0fea
LuisVillalta-Thor <luisvillalta1983@hotmail.com>
2019-07-12 12:23:15 -0600

    Se agrega archivo de Explicacion de Carpetas

---
+++ Proyecto_Tarea3/R/Explicacion_Carpetas_Proyecto.R
@@ -0,0 +1,26 @@
+#Descripcion Paquetes del Proyecto
+
+R
+Almacena código fuente R (aunque puede almacenar código de otros lenguajes de programación como Python, Stan, C++). A lo largo del proceso de análisis debemos aplicar ciertos cálculos y transformaciones que almacenaremos para luego utilizarlo una o varias veces en el proceso de análisis.
+
+#Preparation
+#Lectura de los datos ya estén estos almacenados en CSV.
+
+#Processing
+#Carpeta de Procesado de los datos
+
+#Modeling
+#Analisis Exploratorio de Variables
+
+#Test
+#Carpeta de verificacion de las funciones
+
+#Data/Raw
+#En esta carpeta almacenaremos una copia local de la lectura de datos generada en “preparation”.
+
+#Data/Processed
+#Guardado de forma local una copia de los resultados obtenidos
+
+#Reports
+#Almacenaje de los .Rmd de nuestro reportes así como las imágenes
+#y archivos css requeridos para la creación de los documentos.
---
+++ Proyecto_Tarea3/R/Unbundle_proyecto.R
@@ -0,0 +1,3 @@
+packrat::unbundle("C:/Users/luisv/Desktop/MineriaDatos/Curso.Promidat/2019/ProgramacionAvanzadaR/Clase3/Proyecto_Tarea3/packrat/bundles/Proyecto_Tarea3-2019-07-11.tar.gz",
+  "C:/Users/luisv/Desktop/MineriaDatos/Curso.Promidat/2019/ProgramacionAvanzadaR/Clase3/Proyecto_Tarea3/Tarea3")
+
